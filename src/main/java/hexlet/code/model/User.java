5abd866695772385c185d247757862389f402991
ellonka <ella.trofimova2403@gmail.com>
2023-02-25 08:27:32 +0300

    commit for question about liquibase

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,29 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+}
